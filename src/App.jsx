34b7f9abade9f7312e1908b1f22fa36b4cf2a646
alon <alonlevy5@gmail.com>
2023-02-07 15:15:37 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Experience from './components/experience/Experience';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import Protfolio from './components/protfolio/Protfolio';
+import Services from './components/services/Services';
+import Testemonials from './components/testemonials/Testemonials';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Protfolio />
+      <Testemonials />
+      <Contact />
+      <Footer />
+    </>
+    //30:30
+  );
+}
+
+export default App;
