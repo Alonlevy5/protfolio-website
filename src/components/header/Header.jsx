924af93305c2a4197e12246ff96fa500ba302a44
alon <alonlevy5@gmail.com>
2023-02-12 23:45:49 +0200

    typewritter-effect

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,16 +3,46 @@ import CTA from './CTA';
 import './header.css';
 import ME from '../../assets/me.png';
 import HeaderSocials from './HeaderSocials';
+import Typewriter from 'typewriter-effect';
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Alon Levy</h1>
-        <h5 className="text-light">FullStack Developer</h5>
+        <h5>
+          <Typewriter
+            options={{
+              strings: [`Hello I'm`],
+              autoStart: true,
+              loop: true,
+              pauseFor: 5000,
+            }}
+          />
+        </h5>
+        <h1>
+          <Typewriter
+            options={{
+              strings: [`Alon Levy`],
+              autoStart: true,
+              loop: true,
+              delay: 300,
+              pauseFor: 4500,
+            }}
+          />
+        </h1>
+        <h5 className="text-light">
+          <Typewriter
+            options={{
+              strings: [`FullStack Developer`],
+              autoStart: true,
+              loop: true,
+              delay: 300,
+              pauseFor: 2500,
+            }}
+          />
+        </h5>
         <CTA />
-        <HeaderSocials/>
+        <HeaderSocials />
 
         <div className="me">
           <img src={ME} alt="me" />
